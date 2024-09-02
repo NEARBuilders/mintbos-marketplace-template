659304fdb6dff28b0b5a9a6ad1749384e4a20098
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-09-02 19:36:27 -0400

    fix test

--- playwright.config.js
+++ playwright.config.js
@@ -37,7 +37,7 @@ export default defineConfig({
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:8080",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -45,7 +45,7 @@ export default defineConfig({
       cookies: [],
       origins: [
         {
-          origin: "http://localhost:3000",
+          origin: "http://localhost:8080",
           localStorage: [],
         },
       ],
