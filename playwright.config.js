221b1570258ddc03826d7db4ac33b44f82c8cf0b
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-09-02 19:01:45 -0400

    set playwright dev and port

--- playwright.config.js
+++ playwright.config.js
@@ -95,8 +95,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "yarn start",
-    port: 3000,
+    command: "yarn dev",
+    port: 8080,
     reuseExistingServer: !process.env.CI,
   },
 });
