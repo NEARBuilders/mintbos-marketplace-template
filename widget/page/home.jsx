10c2f24c0fa3ac2303e9e87c7ca0a6398255e0f9
Jiku Godwill <jikugodwill@yahoo.com>
2024-09-26 15:34:02 +0100

    moved fetchNFTInfo to mintbos sdk

--- widget/page/home.jsx
+++ widget/page/home.jsx
@@ -1,6 +1,9 @@
-const { getStoreNFTs } = VM.require(
+const { getStoreNFTs, fetchNFTInfo } = VM.require(
   "${alias_GENADROP}/widget/Mintbase.utils.sdk"
-) || { getStoreNFTs: () => new Promise((resolve) => resolve([])) };
+) || {
+  getStoreNFTs: () => new Promise((resolve) => resolve([])),
+  fetchNFTInfo: () => {},
+};
 
 const { storeId } = props;
 
@@ -74,90 +77,6 @@ return (
               {nftData &&
                 nftData.map((data, index) => {
                   /* will porpbably have to move this to mintbos sdk*/
-                  const fetchNFTInfo = (nftId) => {
-                    const response = fetch(
-                      "https://graph.mintbase.xyz/mainnet",
-                      {
-                        method: "POST",
-                        headers: {
-                          "mb-api-key": "anon",
-                          "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify({
-                          query: `query getTokenByMetadataId {
-          listingsCount: mb_views_active_listings_aggregate(
-              where: {
-                metadata_id: { _eq: "${nftId}" }
-                token: { burned_timestamp: { _is_null: true } }
-              }
-              distinct_on: token_id
-            ) {
-              aggregate {
-                count
-              }
-            }
-              mb_views_nft_tokens(
-                  where: {metadata_id: {_eq: "${nftId}"}}
-                  ) {
-                  media
-                  minter
-                  token_id
-                  metadata_id
-                  splits
-                  royalties_percent
-                  royalties
-                  reference
-                  title
-                  nft_contract_id
-                  owner
-                  base_uri
-                  listings(
-                 where: {unlisted_at: {_is_null: true}, accepted_at: {_is_null: true}, invalidated_at: {_is_null: true}}
-              ) {
-                    price
-                    kind
-                    currency
-                    invalidated_at
-                  }
-                  description
-                  listings_aggregate {
-                      aggregate {
-                      count
-                      }
-                  }
-                  }
-            mb_views_nft_activities_rollup(
-                where: {metadata_id: {_eq: "${nftId}"}}
-                order_by: {timestamp: desc}
-                limit: 200
-              ) {
-                action_receiver
-                action_sender
-                count
-                description
-                kind
-                media
-                metadata_id
-                nft_contract_id
-                receipt_id
-                reference
-                timestamp
-                title
-                tx_sender
-                token_ids
-                price
-              }
-          }
-          `,
-                        }),
-                      }
-                    );
-                    return {
-                      infoNFT: response.body.data.mb_views_nft_tokens[0],
-                      dataTransaction:
-                        response.body.data.mb_views_nft_activities_rollup,
-                    };
-                  };
                   const { infoNFT, dataTransaction } = fetchNFTInfo(
                     data?.metadata_id
                   );
